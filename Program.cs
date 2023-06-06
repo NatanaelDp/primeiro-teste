44fc0fecb59f0f57d04360a693e7c87d9e2d34d1
NatanaelDp <135743045+NatanaelDp@users.noreply.github.com>
2023-06-06 16:22:27 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("uma mensagem de testes");
+            Console.WriteLine("Estou enviando para o GitHub");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
